b30f3b9c435af6961c9c9bc2158401fd18dc9ea0
ashinjohn <ashinbasiljohn@gmail.com>
2021-01-10 23:24:54 +0530

    Added Timer and Control logic for Solar Tank

--- ESPWaterLevel.ino
+++ ESPWaterLevel.ino
@@ -25,7 +25,7 @@ const int SRTrig = D6; // D6 connected to Trigger pin
 float LevelofWater = 0;
 
 // Hardware Mapping
-const int PumpControl = D5;
+const int PumpRelay = D5;
 const int ChargingIndicator = D4;
 const int LevelLED = D8;
 
@@ -45,6 +45,11 @@ Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 // Solar Charging Detection
 bool Charging = false;
 int i = 0;
+
+// Timer Initilisation
+boolean TimerRunning = false;
+uint32_t lastMillis;
+
 void setup() {
   Serial.begin(115200);  // Initialize serial
   Serial.println("Start");
@@ -68,56 +73,94 @@ void setup() {
 
   // Solar Charging Detect pin initialisation
   pinMode(ChargingIndicator, INPUT);
+  // Water Pump Relay pin Initialisation
+  pinMode(PumpRelay, OUTPUT);
+  digitalWrite(PumpRelay, LOW);
+
 }
 
 void loop() {
 
   //Capturing parameters
+  display.clearDisplay(); //Clear splash screen
+  display.setCursor(0, 0);
+  display.print("ESP Water Level");
+
+  LevelofWater = Waterlevel();
+  display.setCursor(1, 0);
+  display.print("Water Level = ");
+  display.print(LevelofWater);
+  display.println(" cm");
+
   Charging = SolarCharging();
+  display.setCursor(2, 0);
+  display.print("Solar = ");
+  display.print(Charging);
+  display.println(" cm");
+  display.display();
+
+  if (LevelofWater >= 120 || LevelofWater <= 0 ) {
+    display.setCursor(4, 0);
+    display.println("Invalid Water Level");
+    display.display();
+    Serial.println("Invalid Water Level");
+    digitalWrite(PumpRelay, LOW);
+  }
+  else if (LevelofWater <= 25)
+  {
+    Serial.println("Water Pump Turned ON");
+    digitalWrite(PumpRelay, HIGH);
+    display.setCursor(3, 0);
+    display.print("Water Pump = ON");
+    display.display();
 
 
-  //  LevelofWater = Waterlevel();
+    // Dry Run Timer set in seconds, Solar tank filling time considered
+    StartTimer();
+    while (1) {
 
+      if (Waterlevel() >= 30)
+      {
+        TimerRunning = false;
+        display.setCursor(4, 0);
+        display.println("No Dry Run");
+        display.display();
+        Serial.println("No Dryrun, Filling Main Tank ");
+        break;
+      }
+      else if ( CheckTimer(600) == true) { // Enter time in seconds - 10 mins
+        display.setCursor(3, 0);
+        display.print("Water Pump = OFF");
+        display.setCursor(4, 0);
+        display.println("Dry Run");
+        display.display();
+        Serial.println("Dry Run");
+        digitalWrite(PumpRelay, LOW);
+        display.display();
+        break;
+      }
+      else {
+        display.setCursor(4, 0);
+        display.println("Filling Tank");
+        display.display();
+        Serial.println("Filling tank, Checking for Dryrun ");
+      }
+      delay(1000);
+    }
+
+    // Overflow Timer set for seconds
+
+  }
 
   Serial.println("bla");
-  display.clearDisplay();
-  display.setCursor(0, 0);
   if (Charging == true) {
     display.print("Solar charging");
   }
   else {
     display.print("No Solar ");
   }
-  display.display();
-  delay(1000);
   //  ESP.deepSleep(5000000);
 
-  // Connect or reconnect to WiFi
-  /*
-    if (WiFi.status() != WL_CONNECTED) {
-      Serial.print("Attempting to connect to SSID: ");
-      Serial.println(ssid);
-      while (WiFi.status() != WL_CONNECTED) {
-        WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
-        Serial.print(".");
-        delay(5000);
-      }
-      Serial.println("\nConnected.");
-    }
-
-    // Write to ThingSpeak. Here, we write to field 1.
-
-
-    int x = ThingSpeak.writeField(myChannelNumber, 1, Waterlevel(), myWriteAPIKey);
-    if (x == 200) {
-      Serial.println("Channel update successful.");
-    }
-    else {
-      Serial.println("Problem updating channel. HTTP error code " + String(x));
-    }
-
-    delay(20000); // Wait 20 seconds to update the channel again
-  */
 }
 
 float Waterlevel() {
@@ -137,7 +180,7 @@ float Waterlevel() {
   TimeofFlight = pulseIn(SREcho, HIGH); // pulseIn gets duration until the pin toggles
   distance = TimeofFlight * 0.034 / 2;
   Serial.print("\nWater Level = ");
-  Serial.print(distance);
+  Serial.print(120 - distance);
   Serial.println(" cm");
   return (120 - distance); //120cm Tank height subtracted to get water level inside tank
 }
@@ -149,9 +192,73 @@ bool SolarCharging() {
     delay(200);
   }
   if ( SolarCharge > 6) {
+    Serial.println("Solar Charging");
     return false;
   }
   else {
+    Serial.println("Solar Not Detected");
     return true;
+  }
+}
+
+bool PushToThingSpeak(float Waterlevel) {
+  // Connect or reconnect to WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
+  }
+
+  // Write to ThingSpeak. Here, we write to field 1.
+
+
+  int x = ThingSpeak.writeField(myChannelNumber, 1, Waterlevel, myWriteAPIKey);
+  if (x == 200) {
+    Serial.println("Channel update successful.");
+    return true;
+  }
+  else {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+    return false;
+  }
+  //    delay(20000); // Wait 20 seconds to update the channel again
+
+}
+
+
+bool CheckTimer(unsigned long TimeOut) {
+  if (TimerRunning && (millis() - lastMillis >= TimeOut * 1000))
+  {
+    TimerRunning = false;
+    Serial.println("Timer End");
+    return true;
+  }
+  else
+  { if  (TimerRunning) {
+      Serial.print("Timer Ticking to");
+      Serial.println(TimeOut * 1000);
+      Serial.println(millis() - lastMillis);
+      display.setCursor(5, 0);
+      display.print(TimeOut * 1000);
+      display.print (" <-- ");
+      display.print(millis() - lastMillis);
+      display.display();
+    }
+    return false;
+  }
+}
+
+void StartTimer() {
+  if (!TimerRunning)
+  {
+    lastMillis = millis();
+    TimerRunning = true;
+    Serial.println("Timer Started");
+    StartTimer();
   }
 }
